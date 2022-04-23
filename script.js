5323ae26400e688d03a858a33af603498eee1550
KaviyaGopi <kaviyagopi002@gmail.com>
2022-04-23 21:24:33 +0530

    Modified

---
+++ script.js
@@ -0,0 +1,67 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const checkTheScore = (msg, status = 'playing', color) => {
+  document.querySelector('.message').textContent = msg;
+  if (status !== 'playing') {
+    // it's making inline style and not changing the css file!
+    document.querySelector('body').style.backgroundColor = color;
+  }
+  if (status === 'win') {
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
+    document.querySelector('.number').textContent = secretNumber;
+  } else if (status === 'lose') {
+    document.querySelector('.score').textContent = 0;
+  } else if (status === 'playing') {
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  document.querySelector('.score').textContent = score;
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (score > 1) {
+    if (guess < 0 || guess > 20) {
+      checkTheScore('Enter number between 0-20');
+    } else if (guess === secretNumber) {
+      checkTheScore('Correct Number', 'win', '#60b347');
+    } else if (guess !== secretNumber)
+      guess < secretNumber
+        ? checkTheScore('Too Low!')
+        : checkTheScore('Too high!');
+  } else {
+    checkTheScore('You lost the game!', 'lose', '#ff0000');
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  console.log('here');
+});
